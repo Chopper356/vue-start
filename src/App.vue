2759d72a1c8e5ad4622c07d736f15a286202d217
Chopper356 <vedmedenko.nikita345@gmail.com>
2021-03-26 01:34:32 +0200

    Empty Project

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,23 @@
 <template>
-	<div id="app">
-		<span >Last date: {{ date }}</span>
-		<Counter :counter="counter"></Counter>
-		<Start :title="title" :content="content" :date="date" :counter="counter" @changeDate="date = $event" @changeCounter="counter = $event"></Start>
+	<div class="container">
+		<form class="pt-3">
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" id="email" class="form-control" v-model="email">
+			</div>
+		</form>
 	</div>
 </template>
 
 <script>
-import Start from './components/Start'
-import Counter from './components/Counter'
 
 export default {
-	name: 'App',
-	data: () => {
-		return {
-			title: "Hello Vue!",
-			content: "Hi!",
-			date: "21.09.2021",
-			counter: 0,
-		}
-	},
-	components: {
-		Start,
-		Counter
-	}
+	data: () => ({
+		email: ""
+	})
 }
 </script>
 
 <style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+
 </style>
